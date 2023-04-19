aa35f47fc65dccfc25e64bf973f2bca653143adb
unknown <ywdblog@gmail.com>
2023-04-19 11:29:54 +0800

    fix

--- pd.py
+++ pd.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*
-
 import pandas as pd
+import sys 
 
+str_gbk = "你好，世界".encode('gbk')
+
+# 输出字节流
+print(str_gbk)
+print(type(str_gbk))
+
+print(str_gbk.decode('utf-8'))
+
+
+sys.exit(0)
+s = b'\xe4\xb8\xad\xe6\x96\x87'
+u = s.decode('utf-8')
+print(u)  # 输出：中文
+
+ 
+ 
 # 读取Excel文件
 df = pd.read_csv('test.csv', nrows=100)
 
